26e7d8ee8d59dbd4bf094e2a7bfe91ae8c989b55
snflkatt <man4project@gmail.com>
2024-10-22 11:00:27 -0500

    request of fuivice.com added 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,11 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-st.text('requrest was imported')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
+# it place data inside container space
+st.data_frame(data=fruityvice_response.json(), use_container_width=True)
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
